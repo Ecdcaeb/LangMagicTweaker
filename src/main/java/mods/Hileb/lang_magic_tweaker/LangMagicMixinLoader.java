8ab41f9b3cc4dd79b3d5f8dd48ff6ee1d6a9e184
Hileb <107909747+Ecdcaeb@users.noreply.github.com>
2025-06-18 10:14:38 +0800

    fix

--- src/main/java/mods/Hileb/lang_magic_tweaker/LangMagicMixinLoader.java
+++ src/main/java/mods/Hileb/lang_magic_tweaker/LangMagicMixinLoader.java
@@ -7,6 +7,8 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.*;
+
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
 public class LangMagicMixinLoader implements zone.rong.mixinbooter.ILateMixinLoader {
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
